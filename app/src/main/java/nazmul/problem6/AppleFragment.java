a97cd367f3232f9b8e91cd6d65dc2eeeaaba9932
najmul_hasan <najmul.hasan10@gmail.com>
2017-04-15 14:57:48 +0600

    Initial commit

---
+++ app/src/main/java/nazmul/problem6/AppleFragment.java
@@ -0,0 +1,44 @@
+package nazmul.problem6;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AppleFragment extends Fragment {
+
+
+    private WebView myWebView;
+
+    public AppleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_google, container, false);
+        myWebView = (WebView) view.findViewById(R.id.help_webview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getActivity(), description, Toast.LENGTH_SHORT).show();
+            }
+        });
+        myWebView.loadUrl("http://www.apple.com/");
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
